94031a1ad3f47204aa778e924f490d0f51185f80
Glauciane Figueiredo20 <104854431+glaucianepeligrini18@users.noreply.github.com>
2024-07-26 11:08:24 -0300

    .

---
+++ main.js
@@ -0,0 +1,46 @@
+const CaixaPrincipal = document.querySelector("caixa principal");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function name() {
+    if ( atual  > perguntas length) {
+        mostre Resultados()
+  return;
+}
+ perguntas Atual =perguntas [ perguntasatual ]
+ caixaPerguntas .textconsten
+
+
+
+
+}
+    
